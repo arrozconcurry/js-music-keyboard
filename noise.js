451ace14035769a563acf101d46072199eef8123
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-22 15:58:09 -0700

    can now play sound back to back

--- noise.js
+++ noise.js
@@ -10,11 +10,22 @@ var sounds = {
 
 $(document).ready( function() {
   // keyDown for c/d/e/f/g/a/b color changes to active
+  $('body').keydown(function() {
 
+  });
   // keyDown plays associated song
   $('body').keydown(function(event) {
     var soundId = sounds[event.keyCode];
-    if (soundId) document.getElementById(soundId).play();
-    else console.log("key not mapped : code is", e.keyCode);
+      // event.keyCode returns the keyCode of the pressed key
+      /* sounds[...] finds value in the sound object by matching
+      the keyCode with the keys in the object.*/
+    var currentSound = document.getElementById(soundId);
+      /* have to call the document to play specified audio
+      since the audio files exist in the HTML*/
+    currentSound.currentTime = 0;
+    currentSound.play();
   });
 });
+
+// borrowed from:
+// https://stackoverflow.com/questions/12578379/play-a-sound-when-a-key-is-pressed
