c4a6cf0ac8e973ac19f2ddf590a78bdcd99cbd09
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-22 15:27:25 -0700

    got something working

--- noise.js
+++ noise.js
@@ -1,3 +1,20 @@
+var sounds = {
+  67 : "cAudio",
+  68 : "dAudio",
+  69 : "eAudio",
+  70 : "fAudio",
+  71 : "gAudio",
+  65 : "aAudio",
+  66 : "bAudio"
+};
+
 $(document).ready( function() {
-  // your code here
+  // keyDown for c/d/e/f/g/a/b color changes to active
+
+  // keyDown plays associated song
+  $('body').keydown(function(event) {
+    var soundId = sounds[event.keyCode];
+    if (soundId) document.getElementById(soundId).play();
+    else console.log("key not mapped : code is", e.keyCode);
+  });
 });
